5e47ec018431d2bf4f10616b69429f29803a7e1f
Matthew <58225683+matthewdamiano66@users.noreply.github.com>
2024-05-09 13:45:17 -0400

    Recent update to this script for deeper conversion
    Updated to convert hours to minutes

--- hours to seconds/converter.py
+++ hours to seconds/converter.py
@@ -2,16 +2,25 @@
 #denotes seconds in an hour 
 global seconds
 seconds = 3600
+#denotes minutes in an hour
+global minute
 minute = 60
+global entry
 
 def how_many_seconds():
+    global entry
     entry = int(input("Please enter the amount of hours you would like to convert:"))
     conversion = entry*seconds
     statement = str(entry)+" "+"hours"+" "+"= "+" "+ str(conversion)+" " +"seconds"
     print(statement)
-    
+
+def how_many_minutes():
+    entry
+    math = minute * entry
+    print(str(entry)+" "+ "hours "+" "+ " ="+" "+str(math) +" " "minutes")
 
 
 
 
 how_many_seconds()
+how_many_minutes()
